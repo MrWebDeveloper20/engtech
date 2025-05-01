b0dc01f4bae66175ee25ae32a67089f6795f7a52
MrWebDeveloper20 <hadihosaini313@gmail.com>
2025-05-01 21:20:31 +0330

    13 commit

--- app/(front)/Research/books.jsx
+++ app/(front)/Research/books.jsx
@@ -77,46 +77,6 @@ const Books = () => {
                 </div>
                 <div className="title flex flex-row-reverse items-center">
                     <h2 className='mr-3'> : عنوان </h2>
-                    <h2 className='mr-3'>کتاب شماره 2</h2>
-                </div>
-                <div className="title flex flex-row-reverse items-center">
-                    <h2 className='mr-3'> : نویسنده </h2>
-                    <h2 className='mr-3'>  دکتر محمد حسینی </h2>
-                </div>
-                <div className="title flex flex-row-reverse items-center">
-                    <h2 className='mr-3'> : موضوع </h2>
-                    <h2 className='mr-3'>  پیشرانه </h2>
-                </div>
-                <button className='bg-blue-500 text-white w-1/2 mx-auto rounded-xl py-2 mb-2'> دانلود  </button>
-
-            </div>
-
-            <div className='w-72 h-fit flex flex-col gap-3 ring-1 rounded-lg'>
-                <div className="img flex justify-center items-center">
-                    <Image src={'/images/book.png'} alt='' width={200} height={200} />
-                </div>
-                <div className="title flex flex-row-reverse items-center">
-                    <h2 className='mr-3'> : عنوان </h2>
-                    <h2 className='mr-3'>کتاب شماره 3</h2>
-                </div>
-                <div className="title flex flex-row-reverse items-center">
-                    <h2 className='mr-3'> : نویسنده </h2>
-                    <h2 className='mr-3'>  دکتر حسین محمدی </h2>
-                </div>
-                <div className="title flex flex-row-reverse items-center">
-                    <h2 className='mr-3'> : موضوع </h2>
-                    <h2 className='mr-3'>  مواد پیشرفته </h2>
-                </div>
-                <button className='bg-blue-500 text-white w-1/2 mx-auto rounded-xl py-2 mb-2'> دانلود  </button>
-
-            </div>
-
-            <div className='w-72 h-fit flex flex-col gap-3 ring-1 rounded-lg'>
-                <div className="img flex justify-center items-center">
-                    <Image src={'/images/book.png'} alt='' width={200} height={200} />
-                </div>
-                <div className="title flex flex-row-reverse items-center">
-                    <h2 className='mr-3'> : عنوان </h2>
                     <h2 className='mr-3'>کتاب شماره 4</h2>
                 </div>
                 <div className="title flex flex-row-reverse items-center">
