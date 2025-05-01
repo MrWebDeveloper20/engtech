6f6b820adc9c73d6d533395e5f3ea15e736a54c6
MrWebDeveloper20 <hadihosaini313@gmail.com>
2025-05-01 14:38:27 +0330

    7 commit

---
+++ app/(front)/Research/books.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Books = () => {
+  return (
+    <div className='w-full grid grid-cols-4 p-4'>
+
+        <div className='w-64 h-80 ring-1'>
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Books
--- app/(front)/Research/page.jsx
+++ app/(front)/Research/page.jsx
@@ -1,18 +1,22 @@
 'use client'
 import React, {useState} from 'react'
+import Books from './books'
 
 const page = () => {
   const [select , setSelect] = useState('book')
   return (
-    <div className='vazir w-full h-screen flex flex-col items-center justify-start my-4'>
-      <div className="text-lg text-gray-600 w-full flex flex-row-reverse justify-center items-center gap-4">
-        <h2 onClick={()=>setSelect('book')} className={` ${select == 'book' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer ring-1 rounded-lg  `}> کتب </h2>
-        <h2 onClick={()=>setSelect('magazine')} className={` ${select == 'magazine' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer ring-1 rounded-lg    `}> نشریات </h2>
-        <h2 onClick={()=>setSelect('article')} className={` ${select == 'article' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer ring-1 rounded-lg  `}> مقالات </h2>
-        <h2 onClick={()=>setSelect('projects')} className={` ${select == 'projects' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer ring-1 rounded-lg  `}> پروژه ها </h2>
-        <h2 onClick={()=>setSelect('Conference')} className={` ${select == 'Conference' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer ring-1 rounded-lg  `}> همایش های علمی </h2>
-        <h2 onClick={()=>setSelect('khedmat')} className={` ${select == 'khedmat' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer ring-1 rounded-lg  `}> کسر | جایگزین خدمت </h2>
-        <h2 onClick={()=>setSelect('Workshop')} className={` ${select == 'Workshop' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer ring-1 rounded-lg  `}> کارگاه ها و دوره های آموزشی   </h2>
+    <div className='vazir w-full h-screen flex flex-row-reverse items-start justify-center m-2'>
+      <div className="w-1/6 text-lg text-gray-600 flex flex-col justify-start items-end gap-4 p-5 ring-1 h-screen rounded-l-lg mr-3">
+        <h2 onClick={()=>setSelect('book')} className={` ${select == 'book' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer rounded-lg  `}> کتب </h2>
+        <h2 onClick={()=>setSelect('magazine')} className={` ${select == 'magazine' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer rounded-lg    `}> نشریات </h2>
+        <h2 onClick={()=>setSelect('article')} className={` ${select == 'article' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer rounded-lg  `}> مقالات </h2>
+        <h2 onClick={()=>setSelect('projects')} className={` ${select == 'projects' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer rounded-lg  `}> پروژه ها </h2>
+        <h2 onClick={()=>setSelect('Conference')} className={` ${select == 'Conference' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer rounded-lg  `}> همایش های علمی </h2>
+        <h2 onClick={()=>setSelect('khedmat')} className={` ${select == 'khedmat' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer rounded-lg  `}> کسر | جایگزین خدمت </h2>
+        <h2 onClick={()=>setSelect('Workshop')} className={` ${select == 'Workshop' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer rounded-lg  `}> کارگاه ها و دوره های آموزشی   </h2>
+      </div>
+      <div className="w-5/6 h-screen ring-1 rounded-r-lg mr-4">
+        {select == 'book' && <Books />}
       </div>
     </div>
   )
