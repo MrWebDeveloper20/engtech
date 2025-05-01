aa1b891f5be9940b863b7ed235f5f50fa795ac43
MrWebDeveloper20 <hadihosaini313@gmail.com>
2025-05-01 14:20:47 +0330

    5 commit

--- app/(front)/ContactUs/page.jsx
+++ app/(front)/ContactUs/page.jsx
@@ -2,8 +2,22 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='w-full h-screen flex flex-col items-center justify-start my-4'>
-      Contact Us
+    <div className='vazir text-xl w-full h-screen flex flex-col items-center justify-start my-4 p-10'>
+      <div className="address w-full flex flex-row-reverse justify-start items-center">
+        <h2 className=''>  : آدرس</h2>
+        <p className='mr-2'> 
+         تهران - بزرگراه شهید بابایی - دانشگاه جامع امام حسین (ع) - موقعیت امام صادق(ع)- دانشكده و پژوهشكده فنی و مهندسی
+        </p>
+      </div>
+      <div className="phone w-full flex flex-row-reverse justify-start items-center">
+        <h2 className=''>  : تلفن تماس</h2>
+        <h2 className='mr-2'> 74189312 </h2>
+      </div>
+      <div className="email w-full flex flex-row-reverse justify-start items-center">
+        <h2 className=''>  : پست الکترونیکی</h2>
+        <h2 className='mr-2'> engtech@ihu.ac.ir </h2>
+      </div>     
+      
     </div>
   )
 }
