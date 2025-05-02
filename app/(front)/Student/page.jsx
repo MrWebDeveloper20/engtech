f5b87ba230405fc6be8a308b5ae0d925743d9ff1
MrWebDeveloper20 <hadihosaini313@gmail.com>
2025-05-02 11:40:16 +0330

    28 commit

--- app/(front)/Student/page.jsx
+++ app/(front)/Student/page.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+'use client'
+import React, {useState} from 'react'
+// import Books from './books'
+// import Mags from './mag'
 
 const page = () => {
+  const [select , setSelect] = useState('basij')
   return (
-    <div className='w-full h-screen flex flex-col items-center justify-start my-4'>
-      دانشجویی
+    <div className='vazir w-full h-screen flex flex-row-reverse items-start justify-center m-2'>
+      <div className="w-1/5 text-gray-600 flex flex-col justify-start items-end gap-4 p-5 ring-1 h-screen rounded-l-lg mr-3">
+        <h2 onClick={()=>setSelect('basij')} className={` ${select == 'basij' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer rounded-lg  `}> بسیج دانشجویی  </h2>
+        <h2 onClick={()=>setSelect('sport')} className={` ${select == 'sport' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer rounded-lg  `}> مسابقات ورزشی  </h2>
+        <h2 onClick={()=>setSelect('association')} className={` ${select == 'association' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer rounded-lg  `}> انحمن های علمی </h2>
+        <h2 onClick={()=>setSelect('second')} className={` ${select == 'second' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer rounded-lg  `}> افق گام دوم انقلاب </h2>
+        <h2 onClick={()=>setSelect('camp')} className={` ${select == 'camp' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer rounded-lg    `}> اردوهای علمی، تربیتی </h2>
+        <h2 onClick={()=>setSelect('Scientific')} className={` ${select == 'Scientific' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer rounded-lg  `}> مسابقات علمی، فرهنگی   </h2>
+        <h2 onClick={()=>setSelect('Cultural')} className={` ${select == 'Cultural' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer rounded-lg  `}> برنامه های فرهنگی، تربیتی </h2>
+      </div>
+      <div className="w-4/5 h-screen ring-1 rounded-r-lg mr-4">
+        {/* {select == 'book' && <Books />} */}
+        {/* {select == 'magazine' && <Mags />} */}
+      </div>
     </div>
   )
 }
