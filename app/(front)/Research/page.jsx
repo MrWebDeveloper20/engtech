85f9a320a2dc62e2b0c3e203309ff9bc337158c5
MrWebDeveloper20 <hadihosaini313@gmail.com>
2025-05-02 11:13:00 +0330

    23 commit

--- app/(front)/Research/page.jsx
+++ app/(front)/Research/page.jsx
@@ -7,7 +7,7 @@ const page = () => {
   const [select , setSelect] = useState('book')
   return (
     <div className='vazir w-full h-screen flex flex-row-reverse items-start justify-center m-2'>
-      <div className="w-1/6 text-lg text-gray-600 flex flex-col justify-start items-end gap-4 p-5 ring-1 h-screen rounded-l-lg mr-3">
+      <div className="w-1/5 text-lg text-gray-600 flex flex-col justify-start items-end gap-4 p-5 ring-1 h-screen rounded-l-lg mr-3">
         <h2 onClick={()=>setSelect('book')} className={` ${select == 'book' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer rounded-lg  `}> کتب </h2>
         <h2 onClick={()=>setSelect('magazine')} className={` ${select == 'magazine' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer rounded-lg    `}> نشریات </h2>
         <h2 onClick={()=>setSelect('article')} className={` ${select == 'article' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer rounded-lg  `}> مقالات </h2>
@@ -16,7 +16,7 @@ const page = () => {
         <h2 onClick={()=>setSelect('khedmat')} className={` ${select == 'khedmat' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer rounded-lg  `}> کسر | جایگزین خدمت </h2>
         <h2 onClick={()=>setSelect('Workshop')} className={` ${select == 'Workshop' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer rounded-lg  `}> کارگاه ها و دوره های آموزشی   </h2>
       </div>
-      <div className="w-5/6 h-screen ring-1 rounded-r-lg mr-4">
+      <div className="w-4/5 h-screen ring-1 rounded-r-lg mr-4">
         {select == 'book' && <Books />}
         {select == 'magazine' && <Mags />}
       </div>
