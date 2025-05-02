edf288594e4c9aa02608aff57e29745f30772e9a
MrWebDeveloper20 <hadihosaini313@gmail.com>
2025-05-02 10:13:36 +0330

    17 commit

--- app/api/engtech/route.ts
+++ app/api/engtech/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 export const GET = async () => {
   // const { car } = data
   const book = await dbClient
-    .db('engtech')
+    .db('rojanoo')
     .collection('books')
     .find()
     .toArray();
