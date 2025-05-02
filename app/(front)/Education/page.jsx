8360751a5c411f8fdbf695ca83d5e2c77c92621e
MrWebDeveloper20 <hadihosaini313@gmail.com>
2025-05-02 11:32:51 +0330

    27 commit

--- app/(front)/Education/page.jsx
+++ app/(front)/Education/page.jsx
@@ -7,7 +7,7 @@ const page = () => {
   const [select , setSelect] = useState('Field')
   return (
     <div className='vazir w-full h-screen flex flex-row-reverse items-start justify-center m-2'>
-      <div className="w-1/5 text text-gray-600 flex flex-col justify-start items-end gap-4 p-5 ring-1 h-screen rounded-l-lg mr-3">
+      <div className="w-1/5 text-gray-600 flex flex-col justify-start items-end gap-4 p-5 ring-1 h-screen rounded-l-lg mr-3">
         <h2 onClick={()=>setSelect('Field')} className={` ${select == 'Field' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer rounded-lg  `}> رشته - گرایش ها و دوره های تحصیلی </h2>
         <h2 onClick={()=>setSelect('roules')} className={` ${select == 'roules' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer rounded-lg  `}> آئین نامه ها، مقررات و بخش نامه ها  </h2>
         <h2 onClick={()=>setSelect('forms')} className={` ${select == 'forms' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer rounded-lg  `}> فرم ها و کاربرگ های آموزشی   </h2>
--- app/(front)/Research/page.jsx
+++ app/(front)/Research/page.jsx
@@ -7,7 +7,7 @@ const page = () => {
   const [select , setSelect] = useState('book')
   return (
     <div className='vazir w-full h-screen flex flex-row-reverse items-start justify-center m-2'>
-      <div className="w-1/5 text-lg text-gray-600 flex flex-col justify-start items-end gap-4 p-5 ring-1 h-screen rounded-l-lg mr-3">
+      <div className="w-1/5 text-gray-600 flex flex-col justify-start items-end gap-4 p-5 ring-1 h-screen rounded-l-lg mr-3">
         <h2 onClick={()=>setSelect('book')} className={` ${select == 'book' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer rounded-lg  `}> کتب </h2>
         <h2 onClick={()=>setSelect('magazine')} className={` ${select == 'magazine' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer rounded-lg    `}> نشریات </h2>
         <h2 onClick={()=>setSelect('article')} className={` ${select == 'article' && 'bg-green-500 text-white px-2 rounded-lg ' }hover:text-blue-800 p-1 cursor-pointer rounded-lg  `}> مقالات </h2>
